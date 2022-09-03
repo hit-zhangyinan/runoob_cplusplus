c3c8e0675ec30a245a72e9ad92c11892b014f819
hit-zhangyinan <z2352173391@outlook.com>
2022-09-03 16:33:26 +0800

    Create relational_operator_overload.cpp

---
+++ overload/relational_operator_overload.cpp
@@ -0,0 +1,40 @@
+// https://www.runoob.com/cplusplus/relational-operators-overloading.html
+// 2022.09.03
+
+#include <iostream>
+
+using namespace std;
+
+class Distance
+{
+private:
+	int feet;
+	int inches;
+
+public:
+	Distance() { feet = 0; inches = 0; }
+	Distance(int f, int i) { feet = f; inches = i; }
+
+	void displayDistance() { cout << "F: " << feet << " I: " << inches << endl; }
+
+	bool operator<(const Distance& d)
+	{
+		if (feet < d.feet)
+			return true;
+		if (feet == d.feet && inches < d.inches)
+			return true;
+		return false;
+	}
+};
+
+int main()
+{
+	Distance d1(11, 10), d2(5, 11);
+
+	if (d1 < d2)
+		cout << "d1 is less than d2" << endl;
+	else
+		cout << "d2 is less than d1" << endl;
+
+	return 0;
+}
