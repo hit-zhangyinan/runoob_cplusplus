333e7b1c690669ac5bc3c568a43a3b29416b8045
hit-zhangyinan <z2352173391@outlook.com>
2022-09-03 14:01:58 +0800

    Create unary_operators_overload.cpp

---
+++ overload/unary_operators_overload.cpp
@@ -0,0 +1,37 @@
+// https://www.runoob.com/cplusplus/unary-operators-overloading.html
+// 2022.09.03
+
+#include <iostream>
+
+using namespace std;
+
+class Distance
+{
+private:
+	int feet;
+	int inches;
+public:
+	Distance() { feet = 0; inches = 0; }
+	Distance(int f, int i) { feet = f; inches = i; }
+	void displayDistance() { cout << "F: " << feet << " I: " << inches << endl; }
+
+	Distance operator-()
+	{
+		feet = -feet;
+		inches = -inches;
+		return Distance(feet, inches);
+	}
+};
+
+int main()
+{
+	Distance d1(11, 10), d2(-5, 11);
+
+	-d1;
+	d1.displayDistance();
+
+	-d2;
+	d2.displayDistance();
+
+	return 0;
+}
